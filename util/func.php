38c9e3cd5d7c4decc24466928f92ede2cb58120e
Pavle Ergovic <pavleergovic@gmai.com>
2025-04-07 02:57:56 +0200

    formatted some code

--- util/func.php
+++ util/func.php
@@ -1,16 +1,17 @@
 <?php
-function animate_text($text) {
+function animate_text($text)
+{
 
-    $html_list = []; 
+    $html_list = [];
     $delay = 0;
     $html_list[] = '<div class="flex-row">';
     for ($i = 0; $i < strlen($text); $i++) {
-        $delay += 100; 
+        $delay += 100;
         $char = mb_substr($text, $i, 1, 'UTF-8');
         if ($char === ' ') {
-            $html_list[] = '</div> <span class="space"></span> <div class="flex-row">'; 
+            $html_list[] = '</div> <span class="space"></span> <div class="flex-row">';
         } else {
-            $html_list[] = '<p data-aos="fade-up" data-aos-delay="' . $delay . '">' . $char . '</p>'; 
+            $html_list[] = '<p data-aos="fade-up" data-aos-delay="' . $delay . '">' . $char . '</p>';
         }
     }
     $html_list[] = "</div>";
